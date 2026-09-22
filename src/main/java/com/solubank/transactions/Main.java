29b746d594e035e90a016677fdc372c63c6e069b
anasselghazi <anasseelghazi10@gmail.com>
2026-09-22 10:14:19 +0000

    build: add maven config and jdbc driver

---
+++ src/main/java/com/solubank/transactions/Main.java
@@ -0,0 +1,8 @@
+package com.solubank.transactions;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Banque Al Baraka - Analyse des Transactions Bancaires");
+        System.out.println("Application demarree avec succes.");
+    }
+}
